30b631802f64241969daa74daa1e7be4215ca49d
iDépanne <69732672+idepanne@users.noreply.github.com>
2020-08-29 14:43:36 +0200

    Update autoupdate_conf.sh

--- autoupdate_conf.sh
+++ autoupdate_conf.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 cd
-echo "autoupdate_conf.sh 2.4.0b2"
+echo "autoupdate_conf.sh 2.4.0b4"
 echo "© 2020 iDépanne – L'expert informatique"
+echo ""
+echo ""
+echo "*** Mise à jour d'autoupdate.sh ***"
+echo ""
+echo "sudo curl https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/autoupdate.sh > autoupdate_new.sh"
+sudo curl https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/autoupdate.sh > autoupdate_new.sh
+echo ""
+echo "sudo mv autoupdate.sh autoupdate_old.sh"
+sudo mv autoupdate.sh autoupdate_old.sh
+echo "sudo mv autoupdate_new.sh autoupdate.sh"
+sudo mv autoupdate_new.sh autoupdate.sh
+echo "sudo chmod +x autoupdate.sh"
+sudo chmod +x autoupdate.sh
+echo "sudo rm -rv autoupdate_old.sh"
+sudo rm -rv autoupdate_old.sh
+echo ""
+echo "*** Execution de autoupdate.sh ***"
 echo ""
 echo ""
 echo "*** Mises à jour du système ***"
