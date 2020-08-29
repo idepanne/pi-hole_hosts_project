5c307d72a328adaafe9c9200265cc795ad98f692
iDépanne <69732672+idepanne@users.noreply.github.com>
2020-08-29 16:28:53 +0200

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd
-echo "autoupdate.sh 2.4.2.1"
+echo "autoupdate.sh 2.4.2.2"
 echo "© 2020 iDépanne – L'expert informatique"
 echo ""
 cat /proc/cpuinfo | grep Model
@@ -27,8 +27,6 @@ echo "sudo mv autoupdate_conf_new.sh autoupdate_conf.sh"
 sudo mv autoupdate_conf_new.sh autoupdate_conf.sh
 echo "sudo chmod +x autoupdate_conf.sh"
 sudo chmod +x autoupdate_conf.sh
-echo "sudo rm -rv autoupdate_conf_old.sh"
-sudo rm -rv autoupdate_conf_old.sh
 echo ""
 echo "*** Execution de autoupdate_conf.sh ***"
 echo ""
