db68b551f0c52f990c4d2ec35a0b897c31face58
iDépanne <69732672+idepanne@users.noreply.github.com>
2020-09-10 17:38:48 +0200

    Update autoupdate.sh

--- apps/autoupdate.sh
+++ apps/autoupdate.sh
@@ -5,8 +5,19 @@ echo "© 2020 iDépanne – L'expert informatique"
 echo "https://idepanne.now.site"
 echo ""
 echo ""
-echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/autoupdate_conf.sh > autoupdate_conf_new.sh"
-wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/autoupdate_conf.sh > autoupdate_conf_new.sh
+echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/infos_system.sh > infos_system_new.sh"
+wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/infos_system.sh > infos_system_new.sh
+echo ""
+echo "sudo mv infos_system.sh infos_system_old.sh"
+sudo mv infos_system.sh infos_system_old.sh
+echo "sudo mv infos_system_new.sh infos_system.sh"
+sudo mv infos_system_new.sh infos_system.sh
+echo "sudo chmod +x infos_system.sh"
+sudo chmod +x infos_system.sh
+echo ""
+echo ""
+echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate_conf.sh > autoupdate_conf_new.sh"
+wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate_conf.sh > autoupdate_conf_new.sh
 echo ""
 echo "sudo mv autoupdate_conf.sh autoupdate_conf_old.sh"
 sudo mv autoupdate_conf.sh autoupdate_conf_old.sh
