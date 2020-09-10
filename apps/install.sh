d5bc90d8edf3389228121e1c95b5fe525734299d
iDépanne <69732672+idepanne@users.noreply.github.com>
2020-09-10 17:39:13 +0200

    Update install.sh

--- apps/install.sh
+++ apps/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 clear
 cd
-echo "install.sh 2.0.0b1
+echo "install.sh 2.0.0b1"
 echo "© 2020 iDépanne – L'expert informatique"
 echo "https://idepanne.now.site"
 echo ""
 echo ""
-echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/infos_system.sh > infos_system.sh"
-wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/infos_system.sh > infos_system.sh
+echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/infos_system.sh > infos_system.sh"
+wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/infos_system.sh > infos_system.sh
 echo ""
 echo "sudo chmod +x infos_system.sh"
 sudo chmod +x infos_system.sh
