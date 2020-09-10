d11e586ad00d72e53ddb53d536d85a1cfab4e227
iDépanne <69732672+idepanne@users.noreply.github.com>
2020-09-10 16:59:05 +0200

    Create infos_system.sh

---
+++ apps/infos_system.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# infos_system.sh 2.0.0b1"
+# © 2020 iDépanne – L'expert informatique
+
+cd
+echo "-------------------------------------------------------------------------------"
+echo "   Informations système"
+echo "-------------------------------------------------------------------------------"
+echo ""
+cat /proc/cpuinfo | grep Model
+cat /proc/cpuinfo | grep Serial
+echo ""
+var1=$(lscpu | grep "Model name:" | sed -r 's/Model name:\s{1,}//g')
+var2=$(lscpu | grep "Vendor ID:" | sed -r 's/Vendor ID:\s{1,}//g')
+echo -n "Processeur      : " && echo "$var2 $var1"
+cpuTemp0=$(cat /sys/class/thermal/thermal_zone0/temp)
+cpuTemp1=$(($cpuTemp0/1000))
+cpuTemp2=$(($cpuTemp0/100))
+cpuTempM=$(($cpuTemp2 % $cpuTemp1))
+echo -n "Température     : "; echo CPU temp"="$cpuTemp1"."$cpuTempM"'C"
+echo -n "                  "; echo GPU $(/opt/vc/bin/vcgencmd measure_temp)
+echo ""
+echo -n "Firmware        : "
+/opt/vc/bin/vcgencmd version
+echo ""
+echo -n "EEPROM          : "
+sudo rpi-eeprom-update
+echo ""
+echo -n "Système         : "; uname -sr
+echo -n "IPv4/IPv6       : "; hostname -I
+echo ""
+var3=$(uptime -s)
+var4=$(uptime -p)
+echo -n "Démarré depuis  : " && echo "$var3 - $var4"
+echo ""
+echo "Stockage        : "
+df -h /
+df -h | grep /var/log
+echo ""
+echo "RAM             : "
+free -ht
