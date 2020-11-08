803106e5caadf9d6e08b0159d5981461fab37d54
iDépanne <69732672+idepanne@users.noreply.github.com>
2020-11-08 18:54:42 +0100

    Update install.sh

--- apps/install.sh
+++ apps/install.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 clear
 cd
-echo "install.sh 3.2.0"
+echo "install.sh 3.3.0"
 echo "© 2020 iDépanne – L'expert informatique"
 echo "https://idepanne.now.site"
 echo ""
 echo ""
+echo "-------------------------------------------------------------------------------"
+echo "   Mise à jour du module infos_system.sh"
+echo "-------------------------------------------------------------------------------"
+echo ""
 echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/infos_system.sh > infos_system.sh"
 wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/infos_system.sh > infos_system.sh
 echo ""
-echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/install2.sh > install2.sh"
-wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/install2.sh > install2.sh
-echo ""
 echo "sudo chmod +x infos_system.sh"
 sudo chmod +x infos_system.sh
+echo ""
+echo ""
+echo "-------------------------------------------------------------------------------"
+echo "   Mise à jour du module install2.sh"
+echo "-------------------------------------------------------------------------------"
+echo ""
+echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/install2.sh > install2.sh"
+wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/install2.sh > install2.sh
 echo ""
 echo "sudo chmod +x install2.sh"
 sudo chmod +x install2.sh
