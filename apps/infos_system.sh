7d41ffc476c7a63f5115ce10479c858c622a8d1f
iDépanne <69732672+idepanne@users.noreply.github.com>
2020-11-14 23:26:50 +0100

    Update infos_system.sh

--- apps/infos_system.sh
+++ apps/infos_system.sh
@@ -10,47 +10,47 @@ echo "--------------------------------------------------------------------------
 echo "   Informations système"
 echo "-------------------------------------------------------------------------------"
 echo ""
-cat /proc/cpuinfo | grep Model
-cat /proc/cpuinfo | grep Serial
+echo "• " && cat /proc/cpuinfo | grep Model
+echo "• " && cat /proc/cpuinfo | grep Serial
 echo ""
 var1=$(lscpu | grep "Model name:" | sed -r 's/Model name:\s{1,}//g')
 var2=$(lscpu | grep "Vendor ID:" | sed -r 's/Vendor ID:\s{1,}//g')
-echo -n "Processeur      : " && echo "$var2 $var1"
+echo -n "• Processeur      : " && echo "$var2 $var1"
 cpuTemp0=$(cat /sys/class/thermal/thermal_zone0/temp)
 cpuTemp1=$(($cpuTemp0/1000))
 cpuTemp2=$(($cpuTemp0/100))
 cpuTempM=$(($cpuTemp2 % $cpuTemp1))
-echo -n "Température     : "; echo CPU temp"="$cpuTemp1"."$cpuTempM"'C"
-echo -n "                  "; echo GPU $(/opt/vc/bin/vcgencmd measure_temp)
+echo -n "• Température     : "; echo CPU temp"="$cpuTemp1"."$cpuTempM"'C"
+echo -n "                    "; echo GPU $(/opt/vc/bin/vcgencmd measure_temp)
 echo ""
-echo -n "Firmware        : "
+echo -n "• Firmware        : "
 /opt/vc/bin/vcgencmd version
 echo ""
-echo -n "EEPROM          : "
+echo -n "• EEPROM          : "
 sudo rpi-eeprom-update
 echo ""
-echo -n "Système         : "; uname -sr
-echo -n "IPv4/IPv6       : "; hostname -I
+echo -n "• Système         : "; uname -sr
+echo -n "• IPv4/IPv6       : "; hostname -I
 echo ""
 var3=$(uptime -s)
 var4=$(uptime -p)
-echo -n "Démarré depuis  : " && echo "$var3 - $var4"
+echo -n "• Démarré depuis  : " && echo "$var3 - $var4"
 echo ""
-echo "Synchronisation de l'horloge :"
+echo "• Synchronisation de l'horloge :"
 sudo systemctl daemon-reload
 timedatectl timesync-status && timedatectl
 echo ""
-echo "Stockage        : "
+echo "• Stockage        : "
 df -h /
 df -h | grep /var/log
 echo ""
-echo "RAM             : "
+echo "• RAM             : "
 free -ht
 echo ""
 
-varfile=/home/pi/install.sh
-if [ -f "$varfile" ]; then
-    ./install2.sh
-else
-    ./autoupdate_conf.sh
-fi
+# varfile=/home/pi/install.sh
+# if [ -f "$varfile" ]; then
+#     ./install2.sh
+# else
+#     ./autoupdate_conf.sh
+# fi
