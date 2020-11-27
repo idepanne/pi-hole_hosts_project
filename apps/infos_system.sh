f21240d4a03653a843db08cac21f6c9e35d92a3d
iDépanne <69732672+idepanne@users.noreply.github.com>
2020-11-27 01:31:17 +0100

    Update infos_system.sh

--- apps/infos_system.sh
+++ apps/infos_system.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# echo "infos_system.sh 4.0.2"
+# echo "infos_system.sh 4.0.3"
 # echo "© 2020 iDépanne – L'expert informatique"
-# echo "https://idepanne.now.site"
+# echo "https://fb.me/idepanne/"
 
 cd
 echo ""
