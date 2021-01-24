121f18eda93a1dca0ae9696679d8849a7e9430f5
iDépanne <69732672+idepanne@users.noreply.github.com>
2021-01-24 22:52:00 +0100

    Update autoupdate_5.sh

--- apps/autoupdate_5.sh
+++ apps/autoupdate_5.sh
@@ -41,40 +41,40 @@ echo "$var"
 echo ""
 echo ""
 if [[ "$var" =~ "0% packet loss" ]]; then
-  echo "Connexion Internet: OK"
-  echo ""
-  echo ""
-  echo "-------------------------------------------------------------------------------"
-  echo "                     Mise à jour du module infos_system.sh                     "
-  echo "-------------------------------------------------------------------------------"
-  echo ""
-  echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/infos_system.sh > infos_system_new.sh"
-  wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/infos_system.sh > infos_system_new.sh
-  echo ""
-  echo "sudo mv infos_system.sh infos_system_old.sh"
-  sudo mv infos_system.sh infos_system_old.sh
-  echo "sudo mv infos_system_new.sh infos_system.sh"
-  sudo mv infos_system_new.sh infos_system.sh
-  echo "sudo chmod +x infos_system.sh"
-  sudo chmod +x infos_system.sh
-  echo ""
-  echo ""
-  echo "-------------------------------------------------------------------------------"
-  echo "                    Mise à jour du module autoupdate_conf.sh                   "
-  echo "-------------------------------------------------------------------------------"
-  echo ""
-  echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate_conf.sh > autoupdate_conf_new.sh"
-  wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate_conf.sh > autoupdate_conf_new.sh
-                echo ""
-                echo "sudo mv autoupdate_conf.sh autoupdate_conf_old.sh"
-                sudo mv autoupdate_conf.sh autoupdate_conf_old.sh
-                echo "sudo mv autoupdate_conf_new.sh autoupdate_conf.sh"
-                sudo mv autoupdate_conf_new.sh autoupdate_conf.sh
-                echo "sudo chmod +x autoupdate_conf.sh"
-                sudo chmod +x autoupdate_conf.sh
-                echo ""
-                ./infos_system.sh
-        else
+    echo "Connexion Internet: OK"
+    echo ""
+    echo ""
+    # echo "-------------------------------------------------------------------------------"
+    # echo "                     Mise à jour du module infos_system.sh                     "
+    # echo "-------------------------------------------------------------------------------"
+    # echo ""
+    # echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/infos_system.sh > infos_system_new.sh"
+    # wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/infos_system.sh > infos_system_new.sh
+    # echo ""
+    # echo "sudo mv infos_system.sh infos_system_old.sh"
+    # sudo mv infos_system.sh infos_system_old.sh
+    # echo "sudo mv infos_system_new.sh infos_system.sh"
+    # sudo mv infos_system_new.sh infos_system.sh
+    # echo "sudo chmod +x infos_system.sh"
+    # sudo chmod +x infos_system.sh
+    # echo ""
+    # echo ""
+    # echo "-------------------------------------------------------------------------------"
+    # echo "                    Mise à jour du module autoupdate_conf.sh                   "
+    # echo "-------------------------------------------------------------------------------"
+    # echo ""
+    # echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate_conf.sh > autoupdate_conf_new.sh"
+    # wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate_conf.sh > autoupdate_conf_new.sh
+    # echo ""
+    echo "sudo mv autoupdate_conf.sh autoupdate_conf_old.sh"
+    sudo mv autoupdate_conf.sh autoupdate_conf_old.sh
+    echo "sudo mv autoupdate_conf_new.sh autoupdate_conf.sh"
+    sudo mv autoupdate_conf_new.sh autoupdate_conf.sh
+    echo "sudo chmod +x autoupdate_conf.sh"
+    sudo chmod +x autoupdate_conf.sh
+    echo ""
+    ./infos_system.sh
+else
                 echo "Connexion Internet: Echec"
                 echo ""
                 echo ""
