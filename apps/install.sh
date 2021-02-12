031e88baafbc40060d057bf712e26a0f0d901531
iDépanne <69732672+idepanne@users.noreply.github.com>
2021-02-12 19:02:10 +0100

    Update install.sh

--- apps/install.sh
+++ apps/install.sh
@@ -28,21 +28,21 @@ echo ""
 echo ""
 echo ""
 echo ""
-echo "                               install.sh 5.0.2                                "
+echo "                               install.sh 5.0.3                                "
 echo "                 © 2020-2021 iDépanne – L'expert informatique                  "
 echo "                            https://fb.me/idepanne/                            "
 echo ""
 echo ""
 echo ""
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo "                              Informations système                             "
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo ""
 cat /proc/cpuinfo | grep Model
 cat /proc/cpuinfo | grep Serial
 echo ""
-var1=$(lscpu | grep "Model name:" | sed -r 's/Model name:\s{1,}//g')
-var2=$(lscpu | grep "Vendor ID:" | sed -r 's/Vendor ID:\s{1,}//g')
+var1=$(lscpu | grep "Model name:" | sed -r 's/Model name :\s{1,}//g')
+var2=$(lscpu | grep "Vendor ID:" | sed -r 's/Vendor ID :\s{1,}//g')
 echo -n "Processeur      : " && echo "$var2 $var1"
 cpuTemp0=$(cat /sys/class/thermal/thermal_zone0/temp)
 cpuTemp1=$(($cpuTemp0/1000))
@@ -64,10 +64,6 @@ var3=$(uptime -s)
 var4=$(uptime -p)
 echo -n "Démarré depuis  : " && echo "$var3 - $var4"
 echo ""
-echo "Synchronisation de l'horloge :"
-sudo systemctl daemon-reload
-timedatectl timesync-status && timedatectl
-echo ""
 echo "Stockage        : "
 df -h /
 df -h | grep /var/log
@@ -75,11 +71,15 @@ echo ""
 echo "RAM             : "
 free -ht
 echo ""
+echo "Synchronisation de l'horloge :"
+sudo systemctl daemon-reload
+timedatectl timesync-status && timedatectl
 echo ""
 echo ""
-echo "-------------------------------------------------------------------------------"
+echo ""
+echo "==============================================================================="
 echo "                      Installation des logiciels prérequis                     "
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo ""
 echo "sudo rm -rv *.sh"
 sudo rm -rv *.sh
@@ -87,13 +87,8 @@ echo ""
 echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate.sh > autoupdate.sh"
 wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate.sh > autoupdate.sh
 echo ""
-echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/infosys.sh > infosys.sh"
-wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/infosys.sh > infosys.sh
-echo ""
 echo "sudo chmod +x autoupdate.sh"
 sudo chmod +x autoupdate.sh
-echo "sudo chmod +x infosys.sh"
-sudo chmod +x infosys.sh
 echo ""
 echo "sudo apt-get update"
 sudo apt-get update
@@ -123,27 +118,27 @@ mkdir log
 echo ""
 echo ""
 echo ""
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo "                            Mise à jour du firmware                            "
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo ""
 echo "sudo rpi-update"
 sudo rpi-update
 echo ""
 echo ""
 echo ""
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo "                            Mise à jour de l'EEPROM                            "
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo ""
 echo "sudo rpi-eeprom-update -a"
 sudo rpi-eeprom-update -a
 echo ""
 echo ""
 echo ""
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo "                            Mises à jour du système                            "
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo ""
 echo "$ sudo apt-get update"
 sudo apt-get update
@@ -159,24 +154,24 @@ sudo apt-get full-upgrade -y
 echo ""
 echo ""
 echo ""
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo "                             Mise à jour du crontab                            "
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo ""
-echo "Ancien crontab:"
+echo "Ancien crontab :"
 crontab -l
 echo ""
 crontab <<<"0 3 * * * /home/pi/autoupdate.sh > /home/pi/log/`date --date="+1day" +"%Y%m%d"`_autoupdate.log 2>&1"
 sudo /etc/init.d/cron restart
 echo ""
-echo "Nouveau crontab:"
+echo "Nouveau crontab :"
 crontab -l
 echo ""
 echo ""
 echo ""
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo "                              Nettoyage du système                             "
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo ""
 echo "$ sudo apt-get autoremove -y"
 sudo apt-get autoremove -y
@@ -198,14 +193,14 @@ find /home/pi/log/* -mtime +31 -exec rm -rv {} \;
 echo ""
 echo ""
 echo ""
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo "                          Redémarrage du Raspberry Pi                          "
-echo "-------------------------------------------------------------------------------"
+echo "==============================================================================="
 echo ""
 echo ""
 echo ""
 duration=$SECONDS
-echo "Durée d'exécution: $(($duration / 60)) min $(($duration % 60)) sec"
+echo "Durée d'exécution : $(($duration / 60)) min $(($duration % 60)) sec"
 echo ""
 sleep 1
 sudo reboot
