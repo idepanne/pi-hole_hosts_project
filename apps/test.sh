b6df3d95d4028aa9397ef534e538db2c3996b6d8
iDépanne <69732672+idepanne@users.noreply.github.com>
2021-04-26 17:40:41 +0200

    Update test.sh

--- apps/test.sh
+++ apps/test.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 	var=$(pihole -v)
 		echo "$var"
-		if [[ "$var" =~ "commande introuvable" ]]; then
-			echo "RIEN !"
-		else
-			echo ""
+		if [[ "$var" =~ "Pi-hole version is" ]]; then
 			echo "$ sudo pihole -g"
 			# sudo pihole -g
+		else
+			echo "RIEN !!!"
 		fi
