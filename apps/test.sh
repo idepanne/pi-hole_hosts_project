aa96b7b549963ff7cfe56488b6f6282e215fbbaa
iDépanne <69732672+idepanne@users.noreply.github.com>
2021-04-27 02:02:27 +0200

    Update test.sh

--- apps/test.sh
+++ apps/test.sh
@@ -3,8 +3,48 @@
 	echo "                   Mises à jour des logiciels supplémentaires                  "
 	echo "==============================================================================="
 	echo ""
+	if [[ -f "/home/pi/autoupdate.sh" ]]; then
+		echo "[   Installé   ]   autoupdate.sh"
+		echo ""
+		echo "$ sudo mv autoupdate.sh autoupdate_old.sh"
+		sudo mv autoupdate.sh autoupdate_old.sh
+		echo ""
+		echo "$ wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate.sh > autoupdate.sh"
+		wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate.sh > autoupdate.sh
+		echo ""
+		echo "$ sudo chmod +x autoupdate.sh"
+		sudo chmod +x autoupdate.sh
+	else
+		echo "[ Non installé ]   autoupdate.sh"
+	fi
+	echo ""
+	echo ""
+	echo ""
+	if [[ -d "/etc/fail2ban" ]]; then
+		echo "[   Installé   ]   Fail2ban"
+		echo ""
+		echo "$ sudo rm -rv /etc/fail2ban/jail.local"
+		sudo rm -rv /etc/fail2ban/jail.local
+		echo ""
+		echo "$ wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/jail.local > jail.local"
+		wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/jail.local > jail.local
+		echo ""
+		echo "$ sudo mv jail.local /etc/fail2ban/jail.local"
+		sudo mv jail.local /etc/fail2ban/jail.local
+		echo ""
+		echo "$ sudo service fail2ban restart"
+		sudo service fail2ban restart
+		echo ""
+		echo "sudo systemctl --no-pager status fail2ban"
+		sudo systemctl --no-pager status fail2ban
+	else
+		echo "[ Non installé ]   Fail2ban"
+	fi
+	echo ""
+	echo ""
+	echo ""
 	if [[ -d "/etc/pihole" ]]; then
-		echo "• Pi-Hole :          [Installé]"
+		echo "[   Installé   ]   Pi-hole"
 		echo ""
 		pihole -v
 		echo ""
@@ -17,7 +57,7 @@
 			sudo pihole -g
 		fi
 	else
-		echo "• Pi-Hole :          [Non installé]"
+		echo "[ Non installé ]   Pi-hole"
 	fi
 	echo ""
 	echo ""
