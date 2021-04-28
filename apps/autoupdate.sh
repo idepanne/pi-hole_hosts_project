e01cc700886308305fc5702891e646d3943bf5f1
iDépanne <69732672+idepanne@users.noreply.github.com>
2021-04-28 09:41:57 +0200

    Add files via upload

--- apps/autoupdate.sh
+++ apps/autoupdate.sh
@@ -83,16 +83,16 @@ if [[ "$var" =~ "0% packet loss" ]]; then
 	echo "                        Mises à jour de Raspberry Pi OS                        "
 	echo "==============================================================================="
 	echo ""
-	echo "sudo apt-get update"
+	echo "$ sudo apt-get update"
 	sudo apt-get update
 	echo ""
-	echo "sudo apt-get upgrade -y"
+	echo "$ sudo apt-get upgrade -y"
 	sudo apt-get upgrade -y
 	echo ""
-	echo "sudo apt-get dist-upgrade -y"
+	echo "$ sudo apt-get dist-upgrade -y"
 	sudo apt-get dist-upgrade -y
 	echo ""
-	echo "sudo apt-get full-upgrade -y"
+	echo "$ sudo apt-get full-upgrade -y"
 	sudo apt-get full-upgrade -y
 	echo ""
 	echo ""
@@ -104,13 +104,13 @@ if [[ "$var" =~ "0% packet loss" ]]; then
 	if [[ -f "/home/pi/autoupdate.sh" ]]; then
 		echo "[INSTALLÉ] autoupdate.sh"
 		echo ""
-		echo "sudo mv autoupdate.sh autoupdate_old.sh"
+		echo "$ sudo mv autoupdate.sh autoupdate_old.sh"
 		sudo mv autoupdate.sh autoupdate_old.sh
 		echo ""
-		echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate.sh > autoupdate.sh"
+		echo "$ wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate.sh > autoupdate.sh"
 		wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate.sh > autoupdate.sh
 		echo ""
-		echo "sudo chmod +x autoupdate.sh"
+		echo "$ sudo chmod +x autoupdate.sh"
 		sudo chmod +x autoupdate.sh
 	else
 		echo "[NON INSTALLÉ] autoupdate.sh"
@@ -121,19 +121,19 @@ if [[ "$var" =~ "0% packet loss" ]]; then
 	if [[ -d "/etc/fail2ban" ]]; then
 		echo "[INSTALLÉ] Fail2ban"
 		echo ""
-		echo "sudo rm -rv /etc/fail2ban/jail.local"
+		echo "$ sudo rm -rv /etc/fail2ban/jail.local"
 		sudo rm -rv /etc/fail2ban/jail.local
 		echo ""
-		echo "wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/jail.local > jail.local"
+		echo "$ wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/jail.local > jail.local"
 		wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/jail.local > jail.local
 		echo ""
-		echo "sudo mv jail.local /etc/fail2ban/jail.local"
+		echo "$ sudo mv jail.local /etc/fail2ban/jail.local"
 		sudo mv jail.local /etc/fail2ban/jail.local
 		echo ""
-		echo "sudo service fail2ban restart"
+		echo "$ sudo service fail2ban restart"
 		sudo service fail2ban restart
 		echo ""
-		echo "sudo systemctl --no-pager status fail2ban"
+		echo "$ sudo systemctl --no-pager status fail2ban"
 		sudo systemctl --no-pager status fail2ban
 	else
 		echo "[NON INSTALLÉ] Fail2ban"
@@ -179,22 +179,25 @@ if [[ "$var" =~ "0% packet loss" ]]; then
 	echo "                           Nettoyage et optimisation                           "
 	echo "==============================================================================="
 	echo ""
-	echo "sudo apt-get autoremove -y"
+	echo "$ sudo apt-get autoremove -y"
 	sudo apt-get autoremove -y
 	echo ""
-	echo "sudo apt-get autoclean -y"
+	echo "$ sudo apt-get autoclean -y"
 	sudo apt-get autoclean -y
 	echo ""
-	echo "sudo apt-get clean -y"
+	echo "$ sudo apt-get clean -y"
 	sudo apt-get clean -y
 	echo ""
-	echo "sudo rm -rv *_old.sh"
+	echo "$ sudo rm -rv *_old.sh"
 	sudo rm -rv *_old.sh
 	echo ""
-	echo "sudo rm -rv test*.sh"
+	echo "$ sudo rm -rv test*.sh"
 	sudo rm -rv test*.sh
 	echo ""
-	echo "cd log && find *.log -mtime +31 -exec rm -rv {} \; && cd"
+	echo "$ cd log && find test*.log -exec rm -rv {} \; && cd"
+	cd log && find test*.log -exec rm -rv {} \; && cd
+	echo ""
+	echo "$ cd log && find *.log -mtime +31 -exec rm -rv {} \; && cd"
 	cd log && find *.log -mtime +31 -exec rm -rv {} \; && cd
 	echo ""
 	echo ""
