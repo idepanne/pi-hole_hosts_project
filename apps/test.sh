be06436ab8c357a75904b0316275dad75b211931
iDépanne <69732672+idepanne@users.noreply.github.com>
2021-04-29 02:55:53 +0200

    Update test.sh

--- apps/test.sh
+++ apps/test.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+		echo "$ wget https://raw.githubusercontent.com/jaykepeters/PSS/master/Pi-hole_SafeSearch.sh"
+		wget https://raw.githubusercontent.com/jaykepeters/PSS/master/Pi-hole_SafeSearch.sh
+		echo ""
+		echo "sudo mv ./Pi-hole_SafeSearch.sh /usr/local/bin/"
+		sudo mv ./Pi-hole_SafeSearch.sh /usr/local/bin/
+		echo ""
+		echo "sudo chmod a+x /usr/local/bin/Pi-hole_SafeSearch.sh"
+		sudo chmod a+x /usr/local/bin/Pi-hole_SafeSearch.sh
+		echo ""
+		echo "sudo Pi-hole_SafeSearch.sh -e"
+		sudo Pi-hole_SafeSearch.sh -e
