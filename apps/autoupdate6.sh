d9d8c1077dce0dbf0ab6860486774be781596ddf
iDépanne <69732672+idepanne@users.noreply.github.com>
2021-05-07 01:07:39 +0200

    Add files via upload

--- apps/autoupdate6.sh
+++ apps/autoupdate6.sh
@@ -2,7 +2,7 @@
 cd
 echo "###############################################################################"
 echo "#                                                                             #"
-echo "#                     Pi-Hole Host Project Updater 6.0.0b7                    #"
+echo "#                     Pi-Hole Host Project Updater 6.0.0b8                    #"
 echo "#                 © 2020-2021 iDépanne – L'expert informatique                #"
 echo "#                           https://fb.me/idepanne/                           #"
 echo "#                            idepanne67@gmail.com                             #"
--- apps/install6.sh
+++ apps/install6.sh
@@ -3,7 +3,7 @@ clear
 cd
 echo "###############################################################################"
 echo "#                                                                             #"
-echo "#                     Pi-Hole Host Project Updater 6.0.0b7                    #"
+echo "#                     Pi-Hole Host Project Updater 6.0.0b8                    #"
 echo "#                 © 2020-2021 iDépanne – L'expert informatique                #"
 echo "#                           https://fb.me/idepanne/                           #"
 echo "#                            idepanne67@gmail.com                             #"
