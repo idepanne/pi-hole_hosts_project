2cfe0a016882029bc3f441cfe8184ccb055a2dd0
iDépanne <69732672+idepanne@users.noreply.github.com>
2021-11-02 21:26:47 +0100

    Add files via upload

--- apps/infosys.sh
+++ apps/infosys.sh
@@ -13,27 +13,28 @@ echo ""
 echo ""
 echo ""
 ###### Définition des variables ######
-var0=$(cat /proc/cpuinfo | grep Hardware | cut -c12-)
-if [[ $var0 == *"BCM"* ]]; then
-	var1="Broadcom"
+var1=$(cat /proc/cpuinfo | grep Hardware | cut -c12-)
+if [[ $var1 == *"BCM"* ]]; then
+	var2="Broadcom"
 fi
-var2=$(lscpu | grep "Model name:" | cut -c22-)
-var3=$(lscpu | grep "Vendor ID:" | cut -c22-)
-var4=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq | rev | cut -c4- | rev)
-var5=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq | rev | cut -c4- | rev)
-var6=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq | rev | cut -c4- | rev)
-var7=$(vcgencmd measure_volts core | cut -c6-)
-var8=$(vcgencmd get_config int | egrep "(gpu_freq)" | cut -c10-)
-var9=$(echo $var8 | rev | cut -c9- | rev)
-var10=$(uname -sr)
-var11=$(uname -m)
-if [[ $var11 == *"aarch64"* ]]; then
-	var12="(64 bits)"
+var3=$(cat /proc/cpuinfo | grep Revision | cut -c12-)
+var4=$(lscpu | grep "Model name:" | cut -c22-)
+var5=$(lscpu | grep "Vendor ID:" | cut -c22-)
+var6=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq | rev | cut -c4- | rev)
+var7=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq | rev | cut -c4- | rev)
+var8=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq | rev | cut -c4- | rev)
+var9=$(vcgencmd measure_volts core | cut -c6-)
+var10=$(vcgencmd get_config int | egrep "(gpu_freq)" | cut -c10-)
+var11=$(echo $var10 | rev | cut -c9- | rev)
+var12=$(uname -sr)
+var13=$(uname -m)
+if [[ $var13 == *"aarch64"* ]]; then
+	var14="(64 bits)"
 else
-	var12="(32 bits)"
+	var14="(32 bits)"
 fi
-var13=$(uptime -s)
-var14=$(uptime -p)
+var15=$(uptime -s)
+var16=$(uptime -p)
 ######################################
 echo "==============================================================================="
 echo "   • A propos de ce Raspberry Pi"
@@ -43,18 +44,18 @@ cat /proc/cpuinfo | grep Model
 echo ""
 cat /proc/cpuinfo | grep Serial
 echo ""
-echo -n "SoC             : " && echo "$var1 $var0"
-echo -n "Processeur      : " && echo "$var3 $var2"
-echo -n "Fréquences      : "; echo "Min $var4 MHz - Cur $var5 MHz - Max $var6 MHz"
-echo -n "Voltage         : "; echo "$var7"
+echo -n "SoC             : " && echo "$var2 $var1 (Rev. $var3)"
+echo -n "Processeur      : " && echo "$var5 $var4"
+echo -n "Fréquences      : "; echo "Min $var6 MHz - Cur $var7 MHz - Max $var8 MHz"
+echo -n "Voltage         : "; echo "$var9"
 echo -n "Température     : "; echo "$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')°C"
 echo ""
 echo -n "GPU RAM         : " && echo "$(vcgencmd get_mem gpu)" | cut -c5-
-echo -n "GPU fréquences  : "; echo "$var9 MHz"
+echo -n "GPU fréquences  : "; echo "$var11 MHz"
 echo -n "Codec H264      : " && echo "$(vcgencmd codec_enabled H264)" | cut -c6-
 echo -n "Codec H265      : " && echo "$(vcgencmd codec_enabled H265)" | cut -c6-
 echo ""
-echo -n "Système         : "; echo "$var10 $var12"
+echo -n "Système         : "; echo "$var12 $var14"
 echo ""
 echo -n "Firmware        : "
 uname -v
@@ -66,7 +67,7 @@ echo -n "IPv4/IPv6       : "; hostname -I
 echo ""
 echo -n "Nom d'hôte      : "; hostname
 echo ""
-echo -n "Démarré depuis  : " && echo "$var13 - $var14"
+echo -n "Démarré depuis  : " && echo "$var15 - $var16"
 echo ""
 echo "Stockage        : "
 df -h
