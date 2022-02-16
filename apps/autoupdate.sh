d1e626bee2071186dab32899dd76a9b5a1e4e9de
iDépanne <69732672+idepanne@users.noreply.github.com>
2022-02-16 23:51:59 +0100

    Add files via upload

--- apps/autoupdate.sh
+++ apps/autoupdate.sh
@@ -2,7 +2,7 @@
 cd ~/Apps
 echo "###############################################################################"
 echo "#                                                                             #"
-echo "#                     Pi-Hole Host Project Updater 8.2.1b2                    #"
+echo "#                     Pi-Hole Host Project Updater 8.2.1b3                    #"
 echo "#                                autoupdate.sh                                #"
 echo "#                 © 2020-2022 iDépanne – L'expert informatique                #"
 echo "#                           https://fb.me/idepanne/                           #"
@@ -14,90 +14,94 @@ echo ""
 echo ""
 var0=$(cat /proc/cpuinfo | grep Model)
 if [[ $var0 == *"Raspberry Pi"* ]]; then
-
-echo "==============================================================================="
-echo "   • Test de la connexion Internet"
-echo "==============================================================================="
-echo ""
-var0=$(ping -c 3 1.1.1.1)
-echo "$var0"
-echo ""
-echo ""
-if [[ "$var0" =~ "0% packet loss" ]]; then
-	echo "Connexion Internet : OK"
-	echo ""
-	echo ""
-	echo ""
-	wget -O - https://raw.githubusercontent.com/idepanne/infosys/master/infosys-rpi.sh > infosys-rpi.sh && sudo chmod +x infosys-rpi.sh && ./infosys-rpi.sh
-	echo ""
-	echo ""
-	echo ""
-	echo "==============================================================================="
-	echo "   • Vérification des connexions actives"
-	echo "==============================================================================="
-	echo ""
-	netstat -t | grep 'ESTABLISHED'
-	echo ""
-	echo ""
-	echo ""
-	echo "==============================================================================="
-	echo "   • Mise à jour de updater.sh"
-	echo "==============================================================================="
-	echo ""
-	echo "$ sudo mv updater.sh updater_old.sh"
-	sudo mv updater.sh updater_old.sh
-	echo ""
-	echo "$ wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/updater.sh > updater.sh"
-	wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/updater.sh > updater.sh
-	echo ""
-	echo "$ sudo chmod +x updater.sh"
-	sudo chmod +x updater.sh
-	./updater.sh
-else
-	echo "Connexion Internet : Echec"
-	echo ""
-	echo ""
-	echo "*** Abandon des mises à jour | Nouvelle tentative dans 24h ***"
-	echo ""
-	if [[ -d "/etc/pihole" ]]; then
-		echo ""
-		echo ""
-		echo "==============================================================================="
-		echo "   • Mise à jour du crontab"
-		echo "==============================================================================="
-		echo ""
-		echo "Ancien crontab :"
-		crontab -l
-		echo ""
-		if [[ -f "/home/pi/Apps/backup.sh" ]]; then
-			crontab <<<"0 3 * * * /home/pi/Apps/autoupdate.sh > /home/pi/Apps/log/`date --date="+1day" +"%Y%m%d"`_autoupdate.log 2>&1
+    echo "==============================================================================="
+    echo "   • Test de la connexion Internet"
+    echo "==============================================================================="
+    echo ""
+    var0=$(ping -c 3 1.1.1.5)
+    echo "$var0"
+    echo ""
+    echo ""
+    if [[ "$var0" =~ "0% packet loss" ]]; then
+    	echo "Connexion Internet : OK"
+    	echo ""
+    	echo ""
+    	echo ""
+    	wget -O - https://raw.githubusercontent.com/idepanne/infosys/master/infosys-rpi.sh > infosys-rpi.sh && sudo chmod +x infosys-rpi.sh && ./infosys-rpi.sh
+    	echo ""
+    	echo ""
+    	echo ""
+    	echo "==============================================================================="
+    	echo "   • Vérification des connexions actives"
+    	echo "==============================================================================="
+    	echo ""
+    	netstat -t | grep 'ESTABLISHED'
+    	echo ""
+    	echo ""
+    	echo ""
+    	echo "==============================================================================="
+    	echo "   • Mise à jour de updater.sh"
+    	echo "==============================================================================="
+    	echo ""
+    	echo "$ sudo mv updater.sh updater_old.sh"
+    	sudo mv updater.sh updater_old.sh
+    	echo ""
+    	echo "$ wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/updater.sh > updater.sh"
+    	wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/updater.sh > updater.sh
+    	echo ""
+    	echo "$ sudo chmod +x updater.sh"
+    	sudo chmod +x updater.sh
+    	./updater.sh
+    else
+    	echo "Connexion Internet : Echec"
+    	echo ""
+    	echo ""
+    	echo "*** Abandon des mises à jour | Nouvelle tentative dans 24h ***"
+    	echo ""
+        var20=$(ls /usr/bin/*session)
+        if [[ $var20 == *"lxsession"* || $var20 == *"openbox"*  || $var20 == *"pipewire-media"* ]]; then
+            sleep 1
+        else
+        	if [[ -d "/home/pi/Apps" ]]; then
+        		echo ""
+    	    	echo ""
+    		    echo "==============================================================================="
+    		    echo "   • Mise à jour du crontab"
+    		    echo "==============================================================================="
+    		    echo ""
+    		    echo "Ancien crontab :"
+    		    crontab -l
+    		    echo ""
+    		    if [[ -f "/home/pi/Apps/backup.sh" ]]; then
+    		    	crontab <<<"0 3 * * * /home/pi/Apps/autoupdate.sh > /home/pi/Apps/log/`date --date="+1day" +"%Y%m%d"`_autoupdate.log 2>&1
 30 3 * * * /home/pi/Apps/backup.sh
 0 4 * * 0 sudo reboot >/dev/null 2>&1"
-		else
-			crontab <<<"0 3 * * * /home/pi/Apps/autoupdate.sh > /home/pi/Apps/log/`date --date="+1day" +"%Y%m%d"`_autoupdate.log 2>&1
+    		    else
+    		    	crontab <<<"0 3 * * * /home/pi/Apps/autoupdate.sh > /home/pi/Apps/log/`date --date="+1day" +"%Y%m%d"`_autoupdate.log 2>&1
 0 4 * * 0 sudo reboot >/dev/null 2>&1"
-		fi
-		sudo /etc/init.d/cron restart
-		echo ""
-		echo "Nouveau crontab :"
-		crontab -l
-		echo ""
-	fi
-fi
-echo ""
-echo ""
-echo "###############################################################################"
-echo "#                                                                             #"
-echo "#                    Mise à jour du Raspberry Pi terminée                     #"
-echo "#                                                                             #"
-echo "###############################################################################"
-echo ""
-sleep 1
+    		    fi
+    		    sudo /etc/init.d/cron restart
+    		    echo ""
+    		    echo "Nouveau crontab :"
+    		    crontab -l
+    		    echo ""
+    	    fi
+        fi
+    fi
+    echo ""
+    echo ""
+    echo "###############################################################################"
+    echo "#                                                                             #"
+    echo "#                    Mise à jour du Raspberry Pi terminée                     #"
+    echo "#                                                                             #"
+    echo "###############################################################################"
+    echo ""
+    sleep 1
 else
-echo "Ce programme de mise à jour n'est compatible qu'avec les Raspberry Pi."
-echo "Il ne peut pas être utilisé sur une autre distribution Linux."
-echo ""
-echo ""
-cd
-sudo rm autoupdate.sh
+    echo "Ce programme de mise à jour n'est compatible qu'avec les Raspberry Pi."
+    echo "Il ne peut pas être utilisé sur une autre distribution Linux."
+    echo ""
+    echo ""
+    cd
+    sudo rm autoupdate.sh
 fi
--- apps/install.sh
+++ apps/install.sh
@@ -3,7 +3,7 @@ clear
 cd
 echo "###############################################################################"
 echo "#                                                                             #"
-echo "#                     Pi-Hole Host Project Updater 8.2.1b2                    #"
+echo "#                     Pi-Hole Host Project Updater 8.2.1b3                    #"
 echo "#                                  install.sh                                 #"
 echo "#                 © 2020-2022 iDépanne – L'expert informatique                #"
 echo "#                           https://fb.me/idepanne/                           #"
@@ -111,7 +111,7 @@ fi
 echo ""
 echo ""
 echo "==============================================================================="
-echo "   • Installation des logiciels pour Raspberry Pi OS (CLI)"
+echo "   • Installation des logiciels (CLI)"
 echo "==============================================================================="
 echo ""
 echo "$ sudo apt-get install -y ca-certificates git binutils dnsutils debian-goodies iftop curl"
@@ -126,7 +126,7 @@ if [[ $var20 == *"lxsession"* || $var20 == *"openbox"*  || $var20 == *"pipewire-
 	echo ""
 	echo ""
 	echo "==============================================================================="
-	echo "   • Installation des logiciels pour Raspberry Pi OS (GUI)"
+	echo "   • Installation des logiciels (GUI)"
 	echo "==============================================================================="
 	echo ""
 	echo "$ sudo apt-get install -y libreoffice libreoffice-l10n-fr libreoffice-help-fr hyphen-fr libreoffice-style* libreoffice-nlpsolver chromium-browser chromium-browser-l10n filezilla gparted hardinfo baobab stacer hplip cups system-config-printer simple-scan rpi-imager"
@@ -140,60 +140,60 @@ if [[ $var20 == *"lxsession"* || $var20 == *"openbox"*  || $var20 == *"pipewire-
 	sudo ln -s /usr/lib/arm-linux-gnueabihf/libGLESv2.so /usr/lib/libbrcmGLESv2.so
 	sudo ln -s /usr/lib/arm-linux-gnueabihf/libEGL.so /usr/lib/libbrcmEGL.so	
 	echo ""
-fi
-echo ""
-echo ""
-#if [[ -d "/etc/pihole" ]]; then
-if [[ -d "/home/pi/Apps" ]]; then
-	echo "==============================================================================="
-	echo "   • Création des dossiers"
-	echo "==============================================================================="
-	echo ""
-	cd
-	echo "$ mkdir Apps"
-	mkdir Apps
-	echo ""
-	cd ~/Apps
-	echo "$ mkdir log"
-	mkdir log
-	echo ""
-	echo ""
-	echo ""
-	echo "==============================================================================="
-	echo "   • Installation de autoupdate.sh"
-	echo "==============================================================================="
-	echo ""
-	echo "$ wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate.sh > autoupdate.sh"
-	wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate.sh > autoupdate.sh
-	echo ""
-	echo "$ sudo chmod +x autoupdate.sh"
-	sudo chmod +x autoupdate.sh
-	echo ""
-	echo ""
-	echo ""
-	echo "==============================================================================="
-	echo "   • Mise à jour du crontab"
-	echo "==============================================================================="
-	echo ""
-	echo "Ancien crontab :"
-	crontab -l
-	echo ""
-	if [[ -f "/home/pi/Apps/backup.sh" ]]; then
-		crontab <<<"0 3 * * * /home/pi/Apps/autoupdate.sh > /home/pi/Apps/log/`date --date="+1day" +"%Y%m%d"`_autoupdate.log 2>&1
+else
+    echo ""
+    echo ""
+    if [[ -d "/home/pi/Apps" ]]; then
+    	echo "==============================================================================="
+	    echo "   • Création des dossiers"
+    	echo "==============================================================================="
+    	echo ""
+    	cd
+	    echo "$ mkdir Apps"
+    	mkdir Apps
+    	echo ""
+    	cd ~/Apps
+    	echo "$ mkdir log"
+    	mkdir log
+    	echo ""
+	    echo ""
+    	echo ""
+    	echo "==============================================================================="
+    	echo "   • Installation de autoupdate.sh"
+    	echo "==============================================================================="
+    	echo ""
+    	echo "$ wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate.sh > autoupdate.sh"
+    	wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/autoupdate.sh > autoupdate.sh
+    	echo ""
+    	echo "$ sudo chmod +x autoupdate.sh"
+    	sudo chmod +x autoupdate.sh
+    	echo ""
+    	echo ""
+    	echo ""
+    	echo "==============================================================================="
+    	echo "   • Mise à jour du crontab"
+    	echo "==============================================================================="
+    	echo ""
+    	echo "Ancien crontab :"
+    	crontab -l
+    	echo ""
+	    if [[ -f "/home/pi/Apps/backup.sh" ]]; then
+	    	crontab <<<"0 3 * * * /home/pi/Apps/autoupdate.sh > /home/pi/Apps/log/`date --date="+1day" +"%Y%m%d"`_autoupdate.log 2>&1
 30 3 * * * /home/pi/Apps/backup.sh
 0 4 * * 0 sudo reboot >/dev/null 2>&1"
-	else
-		crontab <<<"0 3 * * * /home/pi/Apps/autoupdate.sh > /home/pi/Apps/log/`date --date="+1day" +"%Y%m%d"`_autoupdate.log 2>&1
+	    else
+    		crontab <<<"0 3 * * * /home/pi/Apps/autoupdate.sh > /home/pi/Apps/log/`date --date="+1day" +"%Y%m%d"`_autoupdate.log 2>&1
 0 4 * * 0 sudo reboot >/dev/null 2>&1"
-	fi
-	sudo /etc/init.d/cron restart
-	echo ""
-	echo "Nouveau crontab :"
-	crontab -l
-	echo ""
-	echo ""
-	echo ""
+	    fi
+	    sudo /etc/init.d/cron restart
+	    echo ""
+	    echo "Nouveau crontab :"
+	    crontab -l
+	    echo ""
+    fi
 fi
+echo ""
+echo ""
 echo "==============================================================================="
 echo "   • Nettoyage et optimisation"
 echo "==============================================================================="
