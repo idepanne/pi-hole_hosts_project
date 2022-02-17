35aa2e0f5f8833c012a6812cc9c34655cde8c536
iDépanne <69732672+idepanne@users.noreply.github.com>
2022-02-17 19:30:43 +0100

    Add files via upload

--- apps/install.sh
+++ apps/install.sh
@@ -3,7 +3,7 @@ clear
 cd
 echo "###############################################################################"
 echo "#                                                                             #"
-echo "#                    Pi-Hole Host Project Updater 8.2.1b13                    #"
+echo "#                    Pi-Hole Host Project Updater 8.2.1b14                    #"
 echo "#                                  install.sh                                 #"
 echo "#                 © 2020-2022 iDépanne – L'expert informatique                #"
 echo "#                           https://fb.me/idepanne/                           #"
@@ -17,6 +17,7 @@ var0=$(cat /proc/cpuinfo | grep Model)
 if [[ $var0 == *"Raspberry Pi"* ]]; then
 
 wget -O - https://raw.githubusercontent.com/idepanne/infosys/master/infosys.sh > infosys.sh && sudo chmod +x infosys.sh && ./infosys.sh
+echo ""
 echo ""
 echo "==============================================================================="
 echo "   • Vérification des connexions actives"
