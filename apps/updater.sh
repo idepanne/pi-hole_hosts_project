69a5ba8dcfaa2abc8b0be981607e6c9196b5cc28
iDépanne <69732672+idepanne@users.noreply.github.com>
2022-07-07 07:23:41 +0200

    Add files via upload

--- apps/updater.sh
+++ apps/updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Pi-Hole Host Project Updater
 # updater.sh
-# [1098]
+# [1099]
 # © 2020-2022 iDépanne – L'expert informatique
 # idepanne67@gmail.com
 
@@ -118,10 +118,10 @@ cd
 if [[ -f "Apps/backup.sh" ]]; then
 	crontab <<<"0 3 * * * ~/Apps/autoupdate.sh > ~/Apps/log/`date --date="+1day" +"%Y%m%d"`_autoupdate.log 2>&1
 30 3 * * * ~/Apps/backup.sh
-0 4 * * * sudo reboot"
+0 4 * * 1 sudo reboot"
 else
 	crontab <<<"0 3 * * * ~/Apps/autoupdate.sh > ~/Apps/log/`date --date="+1day" +"%Y%m%d"`_autoupdate.log 2>&1
-0 4 * * * sudo reboot"
+0 4 * * 1 sudo reboot"
 fi
 sudo /etc/init.d/cron restart
 echo ""
