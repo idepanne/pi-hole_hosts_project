ef2b9e58ba158f8bdaa8cf5e1bce400082cca935
iDépanne <69732672+idepanne@users.noreply.github.com>
2022-08-25 01:26:19 +0200

    Add files via upload

--- apps/autoupdate.sh
+++ apps/autoupdate.sh
@@ -3,7 +3,7 @@ cd ~/Apps
 echo "+=============================================================================+"
 echo "|                         Pi-Hole Host Project Updater                        |"
 echo "|                                autoupdate.sh                                |"
-echo "|                                   [1111]                                    |"
+echo "|                                   [1114]                                    |"
 echo "|                © 2020-2022 iDépanne – L'expert informatique                 |"
 echo "|                            idepanne67@gmail.com                             |"
 echo "+=============================================================================+"
--- apps/updater.sh
+++ apps/updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Pi-Hole Host Project Updater
 # updater.sh
-# [1111]
+# [1114]
 # © 2020-2022 iDépanne – L'expert informatique
 # idepanne67@gmail.com
 
