2c03dfb645dace05139a0a00bb65076db01baedf
iDépanne <69732672+idepanne@users.noreply.github.com>
2022-09-02 01:40:02 +0200

    Add files via upload

--- apps/install.sh
+++ apps/install.sh
@@ -4,7 +4,7 @@ cd
 echo "+=============================================================================+"
 echo "|                         Pi-Hole Host Project Updater                        |"
 echo "|                                 install.sh                                  |"
-echo "|                                   [1114]                                    |"
+echo "|                                   [1116]                                    |"
 echo "|                © 2020-2022 iDépanne – L'expert informatique                 |"
 echo "|                            idepanne67@gmail.com                             |"
 echo "+=============================================================================+"
