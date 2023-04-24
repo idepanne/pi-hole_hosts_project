4bffca4fb7e733d0407c0191d626ea5727edbc35
iDépanne <69732672+idepanne@users.noreply.github.com>
2023-04-24 18:50:35 +0200

    Add files via upload

--- apps/install.sh
+++ apps/install.sh
@@ -4,7 +4,7 @@ cd || return
 echo "+=============================================================================+"
 echo "|                         Pi-Hole Host Project Updater                        |"
 echo "|                                 install.sh                                  |"
-echo "|                                   [1225]                                    |"
+echo "|                                   [1226]                                    |"
 echo "|                © 2020-2023 iDépanne – L'expert informatique                 |"
 echo "|                        idepanne.support.tech@free.fr                        |"
 echo "+=============================================================================+"
--- apps/updater.sh
+++ apps/updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Pi-Hole Host Project Updater
 # updater.sh
-# [1225]
+# [1226]
 # © 2020-2023 iDépanne – L'expert informatique
 # idepanne.support.tech@free.fr
 
@@ -118,6 +118,7 @@ if [[ -f "Apps/backup.sh" ]]; then
 else
 	echo "4. backup.sh :                  [NON INSTALLÉ]"
 fi
+echo ""
 echo ""
 echo ""
 
