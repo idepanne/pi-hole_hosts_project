1da3a6f3977702f54f19495b064cdf69e137597a
iDépanne <69732672+idepanne@users.noreply.github.com>
2023-04-24 18:17:38 +0200

    Add files via upload

--- apps/autoupdate.sh
+++ apps/autoupdate.sh
@@ -3,7 +3,7 @@ cd ~/Apps || return
 echo "+=============================================================================+"
 echo "|                         Pi-Hole Host Project Updater                        |"
 echo "|                                autoupdate.sh                                |"
-echo "|                                   [1224]                                    |"
+echo "|                                   [1225]                                    |"
 echo "|                © 2020-2023 iDépanne – L'expert informatique                 |"
 echo "|                        idepanne.support.tech@free.fr                        |"
 echo "+=============================================================================+"
--- apps/updater.sh
+++ apps/updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Pi-Hole Host Project Updater
 # updater.sh
-# [1224]
+# [1225]
 # © 2020-2023 iDépanne – L'expert informatique
 # idepanne.support.tech@free.fr
 
@@ -103,6 +103,7 @@ if [[ -d "/etc/pihole" ]]; then
 	fi
 	echo ""
 	sudo timeout 1 pihole -c
+	echo ""
 else
 	echo "3. Pi-hole :                    [NON INSTALLÉ]"
 fi
