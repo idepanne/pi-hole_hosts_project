0bc46468313ac22748538ae934e816340034c024
iDépanne <69732672+idepanne@users.noreply.github.com>
2023-05-07 15:53:02 +0200

    Add files via upload

--- apps/resume.sh
+++ apps/resume.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
+cd ~/Apps || return
+echo "+=============================================================================+"
+echo "|                         Pi-Hole Host Project Updater                        |"
+echo "|                                  resume.sh                                  |"
+echo "|                                   [1229]                                    |"
+echo "|                © 2020-2023 iDépanne – L'expert informatique                 |"
+echo "|                        idepanne.support.tech@free.fr                        |"
+echo "+=============================================================================+"
+echo ""
+echo ""
+echo ""
 
 ###### Définition des variables ######
 var18=$(uptime -s)
 var19=$(uptime -p)
 ######################################
 
-
+echo "+=============================================================================+"
+echo "|  • Résumé de la mise à jour                                                 |"
+echo "+=============================================================================+"
+echo ""
 echo -n "Nom d'hôte      : "; hostname -s
+echo ""
 echo -n "Température     : "; echo "$(vcgencmd measure_temp | grep -E -o '[0-9]*\.[0-9]*')°C"
+echo ""
 echo -n "IPv4/IPv6       : "; hostname -I
+echo ""
 echo -n "Démarré depuis  : "; echo "$var18 - $var19"
-
-
-
+echo ""
+echo ""
+echo ""
 echo "$ sudo fail2ban-client status sshd"
 sudo fail2ban-client status sshd
-
-
+echo ""
+echo ""
+echo ""
 sudo timeout 1 pihole -c
