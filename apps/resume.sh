604844af927cd196c9e5943fa9f04a6ff3484053
iDépanne <69732672+idepanne@users.noreply.github.com>
2023-05-16 12:45:51 +0200

    Add files via upload

--- apps/resume.sh
+++ apps/resume.sh
@@ -3,7 +3,7 @@ cd ~/Apps || return
 echo "+=============================================================================+"
 echo "|                         Pi-Hole Host Project Updater                        |"
 echo "|                                  resume.sh                                  |"
-echo "|                                   [1243]                                    |"
+echo "|                                   [1244]                                    |"
 echo "|                © 2020-2023 iDépanne – L'expert informatique                 |"
 echo "|                        idepanne.support.tech@free.fr                        |"
 echo "+=============================================================================+"
@@ -29,7 +29,11 @@ echo ""
 echo -n "Démarré depuis  : "; echo "$var18 - $var19"
 echo ""
 echo ""
+echo ""
 sudo fail2ban-client status sshd
 echo ""
 echo ""
-sudo timeout 1 pihole -c
+echo ""
+sudo timeout 1 pihole -c  > ~/Apps/temp.txt 2>&1 ; sed -i '1,7d' ~/Apps/temp.txt
+cat ~/Apps/temp.txt
+sudo rm -rv ~/Apps/temp.txt >/dev/null 2>&1
