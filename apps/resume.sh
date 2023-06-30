96141df5c61e65e9f36d9866c3788457830c80be
iDépanne <69732672+idepanne@users.noreply.github.com>
2023-06-30 09:40:27 +0200

    Add files via upload

--- apps/resume.sh
+++ apps/resume.sh
@@ -3,7 +3,7 @@ cd ~/Apps || return
 echo "+=============================================================================+"
 echo "|                         Pi-Hole Host Project Updater                        |"
 echo "|                                  resume.sh                                  |"
-echo "|                                   [1247]                                    |"
+echo "|                                   [1248]                                    |"
 echo "|                © 2020-2023 iDépanne – L'expert informatique                 |"
 echo "|                        idepanne.support.tech@free.fr                        |"
 echo "+=============================================================================+"
@@ -14,6 +14,7 @@ echo ""
 ###### Définition des variables ######
 var18=$(uptime -s)
 var19=$(uptime -p)
+varmac=$(inxi -n | cut -s -f 6-11 -d ':' | cut -c2-)
 ######################################
 
 echo "+=============================================================================+"
@@ -23,6 +24,8 @@ echo ""
 echo -n "Nom d'hôte      : "; hostname -s
 echo ""
 echo -n "Température     : "; echo "$(vcgencmd measure_temp | grep -E -o '[0-9]*\.[0-9]*')°C"
+echo ""
+echo -n "Adresse MAC     : "; echo $varmac
 echo ""
 echo -n "IPv4/IPv6       : "; hostname -I
 echo ""
