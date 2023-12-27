67ec8a73331cf0c03b351a87c9a604ed4ffd8676
iDépanne <69732672+idepanne@users.noreply.github.com>
2023-12-27 01:45:40 +0100

    Add files via upload

--- apps/updater.sh
+++ apps/updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Pi-Hole Host Project Updater
 # updater.sh
-# [1320]
+# [1321]
 # © 2020-2023 iDépanne – L'expert informatique
 # idepanne.support.tech@free.fr
 
@@ -131,6 +131,7 @@ if [[ -d "/etc/pihole" ]]; then
 	echo ""
     echo "$ sudo mv -v /var/www/html/index.lighttpd.html /var/www/html/index.lighttpd.html.old"
     sudo mv -v /var/www/html/index.lighttpd.html /var/www/html/index.lighttpd.html.old
+	echo ""
 	echo ""
 	echo "$ wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/html/index.html > index.html"
 	wget -O - https://raw.githubusercontent.com/idepanne/pi-hole_hosts_project/master/apps/html/index.html > index.html
