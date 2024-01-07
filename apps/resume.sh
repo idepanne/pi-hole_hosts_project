8c63375bda314c0f6f055d8b35915ebc7f56cc74
iDépanne <69732672+idepanne@users.noreply.github.com>
2024-01-07 03:19:36 +0100

    Add files via upload

--- apps/resume.sh
+++ apps/resume.sh
@@ -3,7 +3,7 @@ cd ~/Apps || return
 echo "+=============================================================================+"
 echo "|                         Pi-Hole Host Project Updater                        |"
 echo "|                                  resume.sh                                  |"
-echo "|                                   [1334]                                    |"
+echo "|                                   [1335]                                    |"
 echo "|                © 2020-2024 iDépanne – L'expert informatique                 |"
 echo "|                        idepanne.support.tech@free.fr                        |"
 echo "+=============================================================================+"
@@ -56,31 +56,45 @@ if [[ -d "/etc/pihole" ]]; then
 else
     echo >/dev/null 2>&1
 fi
-if [[ $varserver == *"LISTEN 0.0.0.0 3493"* ]]; then
+if [[ -d "/etc/nut" ]]; then
+    echo ""
     echo ""
     echo ""
     echo "Network UPS Tools :"
     echo ""
-	upsc UPS@localhost ups.model > ~/Apps/temp.txt 2>&1 ; sed -i '1d' ~/Apps/temp.txt
-    echo -n "  • Modèle     :  "; echo "$(cat ~/Apps/temp.txt)"
-    sudo rm -rv ~/Apps/temp.txt >/dev/null 2>&1
-    upsc UPS@localhost ups.status > ~/Apps/temp.txt 2>&1 ; sed -i '1d' ~/Apps/temp.txt
-    echo -n "  • Status     :  "; echo "$(cat ~/Apps/temp.txt)"
-    sudo rm -rv ~/Apps/temp.txt >/dev/null 2>&1
-    upsc UPS@localhost battery.charge > ~/Apps/temp.txt 2>&1 ; sed -i '1d' ~/Apps/temp.txt
-    echo -n "  • Charge     :  "; echo "$(cat ~/Apps/temp.txt)%"
-    sudo rm -rv ~/Apps/temp.txt >/dev/null 2>&1
-    upsc UPS@localhost battery.runtime > ~/Apps/temp.txt 2>&1 ; sed -i '1d' ~/Apps/temp.txt
-    vartime1=$(cat ~/Apps/temp.txt)
-    vartime2=$(echo $(($(($vartime1 - $vartime1/86400*86400))/3600))h $(($(($vartime1 - $vartime1/86400*86400))%3600/60))m $(($(($vartime1 - $vartime1/86400*86400))%60))s)
-    echo -n "  • Autonomie  :  "; echo "$vartime2"
-    sudo rm -rv ~/Apps/temp.txt >/dev/null 2>&1
-    echo ""
-    echo "  • Connexions :"
-    netstat -t | grep 'ESTABLISHED'
-    echo ""
-    echo "  • Status :"
-    sudo systemctl status --no-pager -l nut-server.service
+    if [[ $varserver == *"LISTEN 0.0.0.0 3493"* ]]; then
+	    upsc UPS@localhost ups.model > ~/Apps/temp.txt 2>&1 ; sed -i '1d' ~/Apps/temp.txt
+        echo -n "  • Modèle     :  "; echo "$(cat ~/Apps/temp.txt)"
+        sudo rm -rv ~/Apps/temp.txt >/dev/null 2>&1
+        upsc UPS@localhost ups.status > ~/Apps/temp.txt 2>&1 ; sed -i '1d' ~/Apps/temp.txt
+        echo -n "  • Status     :  "; echo "$(cat ~/Apps/temp.txt)"
+        sudo rm -rv ~/Apps/temp.txt >/dev/null 2>&1
+        upsc UPS@localhost battery.charge > ~/Apps/temp.txt 2>&1 ; sed -i '1d' ~/Apps/temp.txt
+        echo -n "  • Charge     :  "; echo "$(cat ~/Apps/temp.txt)%"
+        sudo rm -rv ~/Apps/temp.txt >/dev/null 2>&1
+        upsc UPS@localhost battery.runtime > ~/Apps/temp.txt 2>&1 ; sed -i '1d' ~/Apps/temp.txt
+        vartime1=$(cat ~/Apps/temp.txt)
+        vartime2=$(echo $(($(($vartime1 - $vartime1/86400*86400))/3600))h $(($(($vartime1 - $vartime1/86400*86400))%3600/60))m $(($(($vartime1 - $vartime1/86400*86400))%60))s)
+        echo -n "  • Autonomie  :  "; echo "$vartime2"
+        sudo rm -rv ~/Apps/temp.txt >/dev/null 2>&1
+        echo ""
+        echo "  • Connexions :"
+        netstat -t | grep 'ESTABLISHED'
+        echo ""
+        sudo systemctl restart nut-server
+        sleep 2
+        sudo systemctl restart nut-monitor
+        sleep 2
+        echo "  • Status :"
+        sudo systemctl status --no-pager -l nut-server.service
+        echo ""
+        sudo systemctl status --no-pager -l nut-monitor.service
+    else
+        sudo systemctl restart nut-monitor
+        sleep 2
+        echo "  • Status :"
+        sudo systemctl status --no-pager -l nut-monitor.service
+    fi
 else
     echo >/dev/null 2>&1
 fi
