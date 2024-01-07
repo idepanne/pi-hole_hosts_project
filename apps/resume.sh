10bcb4a9c7f95aee1e12a0d7a84ee06b2b66e06d
iDépanne <69732672+idepanne@users.noreply.github.com>
2024-01-07 03:23:15 +0100

    Add files via upload

--- apps/resume.sh
+++ apps/resume.sh
@@ -3,7 +3,7 @@ cd ~/Apps || return
 echo "+=============================================================================+"
 echo "|                         Pi-Hole Host Project Updater                        |"
 echo "|                                  resume.sh                                  |"
-echo "|                                   [1335]                                    |"
+echo "|                                   [1337]                                    |"
 echo "|                © 2020-2024 iDépanne – L'expert informatique                 |"
 echo "|                        idepanne.support.tech@free.fr                        |"
 echo "+=============================================================================+"
